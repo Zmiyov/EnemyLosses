e82f14c33b8c807831c04515fc04845a9161c5f9
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-07-13 12:09:22 +0300

    Added models, table view controller and view controller

---
+++ EnemyLosses/Equipment.swift
@@ -0,0 +1,40 @@
+//
+//  Equipment.swift
+//  EnemyLosses
+//
+//  Created by Vladimir Pisarenko on 13.07.2022.
+//
+
+import Foundation
+
+struct Equipment: Codable {
+    var date: String
+    var day: Int
+    var aircraft: Int
+    var helicopter: Int
+    var tank: Int
+    var apc: Int
+    var fieldArtillery: Int
+    var mrl: Int
+    var militaryAuto: Int
+    var fuelTank: Int
+    var drone: Int
+    var navalShip: Int
+    var antiAircaftWareFare: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case day
+        case aircraft
+        case helicopter
+        case tank
+        case apc = "APC"
+        case fieldArtillery = "field artillery"
+        case mrl = "MRL"
+        case militaryAuto = "military auto"
+        case fuelTank = "fuel tank"
+        case drone
+        case navalShip = "naval ship"
+        case antiAircaftWareFare = "anti-aircraft warfare"
+    }
+}
---
+++ EnemyLosses/Personnel.swift
@@ -0,0 +1,23 @@
+//
+//  Personnel.swift
+//  EnemyLosses
+//
+//  Created by Vladimir Pisarenko on 13.07.2022.
+//
+
+import Foundation
+
+struct Personnel: Codable {
+    var date: String
+    var day: Int
+    var personnel: Int
+    //          "personnel*": "about",
+    var pow: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case day
+        case personnel
+        case pow = "POW"
+    }
+}
